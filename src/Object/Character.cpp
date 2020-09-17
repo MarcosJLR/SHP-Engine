4bba098f5fc1fbc16642c6362163cdeb5cbe1719
MarcosJLR <socram.693@gmail.com>
2020-09-17 16:59:33 -0400

    Passing orientation in degrees instead of radians

--- src/Object/Character.cpp
+++ src/Object/Character.cpp
@@ -12,7 +12,7 @@ namespace shp
                                             (int) m_Transform.z,
                                             m_Width, 
                                             m_Depth, 
-                                            m_Kinematic.GetOrientation(), 
+                                            RadiansToDegrees(m_Kinematic.GetOrientation()), 
                                             1.0 + m_Transform.y / 32.0);
     }
 
