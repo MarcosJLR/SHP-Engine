a8315fee488705ed2e27437e0665a0baff95e65b
MarcosJLR <socram.693@gmail.com>
2020-09-29 00:52:09 -0400

    Adjusted main engine accordingly

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -7,6 +7,11 @@
 #include "Player.hpp"
 #include "Enemy.hpp"
 #include "Graph.hpp"
+#include "PrisonButton.hpp"
+#include "FranMachine.hpp"
+#include "GreenMachine.hpp"
+#include "RedMachine.hpp"
+#include "NicoMachine.hpp"
 
 namespace shp
 {
@@ -40,13 +45,48 @@ namespace shp
 
         Graph::GetInstance()->Load("assets/map/Map.1");
 
-        TextureManager::GetInstance()->Load("player", "assets/img/Player.png");
-        TextureManager::GetInstance()->Load("enemy", "assets/img/Enemy.png");
-        GameObject* player = new Player(new ObjectProperties({100,50,100}, 32, 32, 32, "player"));
-        GameObject* enemy = new Enemy(new ObjectProperties({500,5,500}, 32, 32, 32, "enemy"), (Character*) player);
+        TextureManager::GetInstance()->Load("nico", "assets/img/Nico.png");
+        TextureManager::GetInstance()->Load("nico-escort", "assets/img/NicoEscort.png");
+        TextureManager::GetInstance()->Load("nico-flee", "assets/img/NicoScared.png");
+        TextureManager::GetInstance()->Load("nico-dead", "assets/img/NicoWasted.png");
+        TextureManager::GetInstance()->Load("juan", "assets/img/Juan.png");
+        TextureManager::GetInstance()->Load("fran-locked", "assets/img/FranMolestico.png");
+        TextureManager::GetInstance()->Load("fran-free", "assets/img/Fran.png");
+        TextureManager::GetInstance()->Load("fran-nico", "assets/img/FranLove.png");
+        TextureManager::GetInstance()->Load("red", "assets/img/Enemy.png");
+        TextureManager::GetInstance()->Load("green", "assets/img/Player.png");
+        TextureManager::GetInstance()->Load("button-pushed", "assets/img/GreenButton.png");
+        TextureManager::GetInstance()->Load("button-unpushed", "assets/img/RedButton.png");
 
-        m_GameObjects.push_back(player);
-        m_GameObjects.push_back(enemy);
+        m_ChangTimeLeft = CHANG_TIME;
+        m_ChangRecharge = -1.0;
+        m_ChangActive = false;
+
+        m_ButtonPushed = false;
+        m_ButtonPosition = new Kinematic({944, 0, 416}, 0.0, 0.0, 0.0);
+        m_ZonePosition = new Kinematic({64, 0, 64}, 0.0, 0.0, 0.0);
+
+        m_NicoDead = false;
+
+        PrisonButton* button = new PrisonButton(new ObjectProperties({944, 0, 416}, 32, 32, 32, "button-unpushed"));
+
+        m_Nico = new StateCharacter(new ObjectProperties({100, 5, 100}, 32, 32, 32, "nico"), nullptr);
+        m_Fran = new StateCharacter(new ObjectProperties({480, 5, 480}, 32, 32, 32, "fran-locked"), nullptr);
+        //m_Juan = new StateCharacter(new ObjectProperties({100, 5, 100}, 32, 32, 32, "juan"), nullptr);
+        m_Red = new StateCharacter(new ObjectProperties({1150, 5, 768}, 32, 32, 32, "red"), nullptr);
+        m_Green = new StateCharacter(new ObjectProperties({1150, 5, 768}, 32, 32, 32, "green"), nullptr);
+
+        m_Nico->SetMachine(new NicoMachine());
+        m_Fran->SetMachine(new FranMachine());
+        //m_Juan->SetMachine(new JuanMachine());
+        m_Red->SetMachine(new RedMachine());
+        m_Green->SetMachine(new GreenMachine());
+
+        m_GameObjects.push_back(m_Nico);
+        m_GameObjects.push_back(m_Fran);
+        m_GameObjects.push_back(m_Red);
+        m_GameObjects.push_back(m_Green);
+        m_GameObjects.push_back(button);
 
         return m_isRunning = true;
     }
@@ -72,13 +112,40 @@ namespace shp
         InputHandler::GetInstance()->Listen();
     }
 
+    void Engine::UpdateChang(double dt)
+    {
+        if(m_ChangActive) 
+        { 
+            m_ChangTimeLeft -= dt; 
+            if(m_ChangTimeLeft < 0.0)
+            {
+                m_ChangRecharge = CHANG_RECHARGE;
+                m_ChangActive = false;
+            } 
+        }
+
+        if(m_ChangRecharge >= 0.0)
+        {
+            m_ChangRecharge -= dt;
+            if(m_ChangRecharge < 0.0)
+            {
+                m_ChangTimeLeft = CHANG_TIME;
+            }
+        }
+    }
+
     void Engine::Update(){
+        double dt = Timer::GetInstance()->GetDeltaTime();
+        
+        UpdateChang(dt);
+
         for(GameObject* object : m_GameObjects)
-            object->Update(Timer::GetInstance()->GetDeltaTime());
+            object->Update(dt);
     }
 
     void Engine::Render(){
         SDL_SetRenderDrawColor(m_Renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        if(m_ChangActive) { SDL_SetRenderDrawColor(m_Renderer, 0x00, 0xFF, 0x00, 0xFF); }
         SDL_RenderClear(m_Renderer);
 
         Graph::GetInstance()->Draw();
--- src/Core/Engine.hpp
+++ src/Core/Engine.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "GameObject.hpp"
+#include "StateCharacter.hpp"
 
 namespace shp
 {
@@ -31,11 +32,33 @@ namespace shp
             inline bool isRunning() { return m_isRunning; }
             inline SDL_Renderer* GetRenderer() { return m_Renderer; }
 
+            inline StateCharacter* GetNico() { return m_Nico; }
+            inline StateCharacter* GetFran() { return m_Fran; }
+            inline StateCharacter* GetJuan() { return m_Juan; }
+            inline StateCharacter* GetRed() { return m_Red; }
+            inline StateCharacter* GetGreen() { return m_Green; }
+
+            inline void ActivateChang() { m_ChangActive = true; }
+            inline bool ChangAvailable() { return m_ChangTimeLeft > 0; }
+            inline bool ChangActive() { return m_ChangActive; }
+
+            void UpdateChang(double dt);
+
+            inline bool IsFranFree() { return m_ButtonPushed; }
+            inline void FreeFran() { m_ButtonPushed = true; }
+            inline void LockFran() { m_ButtonPushed = false; }
+            inline Kinematic* GetButtonPosition() { return m_ButtonPosition; }
+            inline Kinematic* GetZonePosition() { return m_ZonePosition; }
+
+            inline bool IsNicoDead() { return m_NicoDead; }
+            inline void KillNico() { m_NicoDead = true; }
+            inline void ReviveNico() { m_NicoDead = false; }
+
         private:
             Engine() 
                 : m_isRunning(false), 
                   m_Window(nullptr),
-                  m_Renderer(nullptr) 
+                  m_Renderer(nullptr)
             {}
 
             bool m_isRunning;
@@ -44,6 +67,24 @@ namespace shp
             static Engine* s_Instance;
 
             std::vector<GameObject*> m_GameObjects;
+            StateCharacter* m_Nico;
+            StateCharacter* m_Fran;
+            StateCharacter* m_Juan;
+            StateCharacter* m_Red;
+            StateCharacter* m_Green;
 
+            bool m_ChangActive;
+            double m_ChangTimeLeft;
+            double m_ChangRecharge;
+
+            bool m_ButtonPushed;
+            Kinematic* m_ButtonPosition;
+
+            Kinematic* m_ZonePosition;
+
+            bool m_NicoDead; 
+
+            const double CHANG_TIME = 500.0;
+            const double CHANG_RECHARGE = 1500.0;
     };
 };
